0ea20ee6da998c659abfe3f669927075b7a73974
Eric Berquist <eric.berquist@gmail.com>
2017-04-21 12:23:33 -0400

    really clean version of the nn-qm7 code

--- quantum_machine_datasets/nn-qm7/nntest.py
+++ quantum_machine_datasets/nn-qm7/nntest.py
@@ -1,4 +1,12 @@
-import os,pickle,sys,numpy,copy,scipy,scipy.io
+from __future__ import print_function
+
+import os
+import pickle
+import sys
+import numpy as np
+import scipy
+import scipy.io
+
 
 # --------------------------------------------
 # Parameters
@@ -8,27 +16,28 @@ split = int(sys.argv[1]) # test split for cross-validation (between 0 and 5)
 # --------------------------------------------
 # Load data and models
 # --------------------------------------------
-if not os.path.exists('qm7.mat'): os.system('wget http://www.quantum-machine.org/data/qm7.mat')
+if not os.path.exists('qm7.mat'):
+    os.system('wget http://www.quantum-machine.org/data/qm7.mat')
 dataset = scipy.io.loadmat('qm7.mat')
-nn = pickle.load(open('nn-%d.pkl'%split,'r'))
+with open('nn-%d.pkl' % split) as fh:
+    nn = pickle.load(fh)
 
 print('results after %d iterations'%nn.nbiter)
 
-Ptrain = dataset['P'][range(0,split)+range(split+1,5)].flatten()
-Ptest  = dataset['P'][split]
+Ptrain = dataset['P'][range(0, split) + range(split + 1, 5)].flatten()
+Ptest = dataset['P'][split]
 
-for P,name in zip([Ptrain,Ptest],['training','test']):
-	# --------------------------------------------
-	# Extract test data
-	# --------------------------------------------
-	X = dataset['X'][P]
-	T = dataset['T'][0,P]
+for P, name in zip([Ptrain, Ptest], ['training', 'test']):
+    # --------------------------------------------
+    # Extract test data
+    # --------------------------------------------
+    X = dataset['X'][P]
+    T = dataset['T'][0, P]
 
-	# --------------------------------------------
-	# Test the neural network
-	# --------------------------------------------
-	print('\n%s set:'%name)
-	Y = numpy.array([nn.forward(X) for _ in range(10)]).mean(axis=0)
-	print('MAE:  %5.2f kcal/mol'%numpy.abs(Y-T).mean(axis=0))
-	print('RMSE: %5.2f kcal/mol'%numpy.square(Y-T).mean(axis=0)**.5)
-
+    # --------------------------------------------
+    # Test the neural network
+    # --------------------------------------------
+    print('\n%s set:' % name)
+    Y = np.array([nn.forward(X) for _ in range(10)]).mean(axis=0)
+    print('MAE:  %5.2f kcal/mol' % np.abs(Y-T).mean(axis=0))
+    print('RMSE: %5.2f kcal/mol' % np.square(Y-T).mean(axis=0) ** 0.5)
--- quantum_machine_datasets/nn-qm7/nntrain.py
+++ quantum_machine_datasets/nn-qm7/nntrain.py
@@ -1,52 +1,75 @@
-import os,pickle,sys,numpy,nn,copy,scipy,scipy.io
+from __future__ import print_function
+
+import os
+import pickle
+import sys
+import numpy as np
+import copy
+import scipy
+import scipy.io
+
+import nn
+
 
 # --------------------------------------------
 # Parameters
 # --------------------------------------------
-seed  = 3453
+seed = 3453
 split = int(sys.argv[1]) # test split
 
-mb    = 25     # size of the minibatch
-hist  = 0.1    # fraction of the history to be remembered
+mb = 25       # size of the minibatch
+hist = 0.1    # fraction of the history to be remembered
 
 # --------------------------------------------
 # Load data
 # --------------------------------------------
-numpy.random.seed(seed)
-if not os.path.exists('qm7.mat'): os.system('wget http://www.quantum-machine.org/data/qm7.mat')
+np.random.seed(seed)
+if not os.path.exists('qm7.mat'):
+    os.system('wget http://www.quantum-machine.org/data/qm7.mat')
 dataset = scipy.io.loadmat('qm7.mat')
 
 # --------------------------------------------
 # Extract training data
 # --------------------------------------------
-P = dataset['P'][range(0,split)+range(split+1,5)].flatten()
+P = dataset['P'][range(0, split) + range(split + 1, 5)].flatten()
 X = dataset['X'][P]
-T = dataset['T'][0,P]
+T = dataset['T'][0, P]
 
 # --------------------------------------------
 # Create a neural network
 # --------------------------------------------
-I,O = nn.Input(X),nn.Output(T)
-nnsgd = nn.Sequential([I,nn.Linear(I.nbout,400),nn.Sigmoid(),nn.Linear(400,100),nn.Sigmoid(),nn.Linear(100,O.nbinp),O])
+I = nn.Input(X)
+O = nn.Output(T)
+nnsgd = nn.Sequential([
+    I,
+    nn.Linear(I.nbout, 400),
+    nn.Sigmoid(),
+    nn.Linear(400, 100),
+    nn.Sigmoid(),
+    nn.Linear(100, O.nbinp),
+    O
+])
 nnsgd.modules[-2].W *= 0
 nnavg = copy.deepcopy(nnsgd)
 
 # --------------------------------------------
 # Train the neural network
 # --------------------------------------------
-for i in range(1,1000001):
+for i in range(1, 1000001):
 
-	if i > 0:     lr = 0.001  # learning rate
-	if i > 500:   lr = 0.0025
-	if i > 2500:  lr = 0.005
-	if i > 12500: lr = 0.01
+    # learning rate
+    if i > 0: lr = 0.001
+    if i > 500: lr = 0.0025
+    if i > 2500: lr = 0.005
+    if i > 12500: lr = 0.01
 
-	r = numpy.random.randint(0,len(X),[mb])
-	Y = nnsgd.forward(X[r])
-	nnsgd.backward(Y-T[r])
-	nnsgd.update(lr)
-	nnavg.average(nnsgd,(1/hist)/((1/hist)+i))
-	nnavg.nbiter = i
+    r = np.random.randint(0, len(X), [mb])
+    Y = nnsgd.forward(X[r])
+    nnsgd.backward(Y - T[r])
+    nnsgd.update(lr)
+    nnavg.average(nnsgd, (1/hist) / ((1 / hist) + i))
+    nnavg.nbiter = i
 
-	if i % 100 == 0: pickle.dump(nnavg,open('nn-%d.pkl'%split,'w'),pickle.HIGHEST_PROTOCOL)
-
+    if i % 100 == 0:
+        with open('nn-%d.pkl' % split, 'w') as fh:
+            pickle.dump(nnavg, fh, pickle.HIGHEST_PROTOCOL)
